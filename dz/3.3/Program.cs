bfa7dec4acdfd60389762635438eafa125ab9b88
Julianne <U_sub@mail.ru>
2023-03-18 14:04:50 +0300

    m

---
+++ dz/3.3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int ind = 0;
+            int[] a = { 2, 0,-9, 7, 8, -6,-3 };
+            int n = a.Length;
+
+            for (int i = 0; i < n - 1;i++ )
+            {
+                if ((a[i] >= 0 && a[i+1] >= 0) || (a[i] <= 0 && a[i + 1] <= 0))
+                {
+                    ind = i + 1;
+                    break;
+                }
+            }
+            Console.WriteLine(ind);
+
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
